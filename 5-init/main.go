75c539fd7d5d33eb60076d4a614102b7787540ab
xushengjie <xushengjie2014@xiaochuankeji.cn>
2025-07-06 17:09:40 +0800

    34

--- 5-init/main.go
+++ 5-init/main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	mylib2 "GolangStudy/5-init/lib2"
-	"github.com/Xsj234/GolangStudy/5-init/lib"
+	myLib1 "github.com/Xsj234/GolangStudy/5-init/lib1"
+	mylib2 "github.com/Xsj234/GolangStudy/5-init/lib2"
 	//. "GolangStudy/5-init/lib2"
 )
 
 func main() {
-	//lib1.lib1Test()
+	myLib1.Lib1Test()
 
 	//lib2.Lib2Test()
 	mylib2.Lib2Test()
