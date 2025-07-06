05ecab9296ad81f1132d899879cfeba30deab6ff
xushengjie <xushengjie2014@xiaochuankeji.cn>
2025-07-06 17:26:43 +0800

    34

--- 5-init/main.go
+++ 5-init/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	myLib1 "github.com/Xsj234/GolangStudy/5-init/lib1"
 	mylib2 "github.com/Xsj234/GolangStudy/5-init/lib2"
 	//. "GolangStudy/5-init/lib2"
@@ -8,7 +9,7 @@ import (
 
 func main() {
 	myLib1.Lib1Test()
-
+	fmt.Printf("ssss")
 	//lib2.Lib2Test()
 	mylib2.Lib2Test()
 	//Lib2Test()
